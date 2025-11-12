53fd9737f08441f3dd4f14acc71981763cca0f02
Sam5M <samuel.michaud@go.winona.edu>
2025-11-12 13:21:06 -0600

    fix weapon select menus

--- js.js
+++ js.js
@@ -1,9 +1,24 @@
-/* Set the width of the side navigation to 250px */
+/* -------------------- */
+/* SIDENAV FUNCTIONALITY */
+/* -------------------- */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+/* -------------------- */
+/* MENU SELECTION LOGIC */
+/* -------------------- */
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".menu").forEach(menu => {
+        menu.addEventListener("click", e => {
+            if (e.target.tagName === "BUTTON") {
+                menu.querySelectorAll("button").forEach(btn => btn.classList.remove("active"));
+                e.target.classList.add("active");
+            }
+        });
+    });
+});
